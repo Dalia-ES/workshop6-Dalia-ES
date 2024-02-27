be790a7f2a8ca7540dc59a0b982ac41af34270f1
Dalia-ES <d.el-shoura@se20.qmul.ac.uk>
2024-02-27 12:55:42 +0000

    Use filter function

--- Workshop 6.R
+++ Workshop 6.R
@@ -10,3 +10,12 @@ beetles %>% select(1:68) #Selects species columns by numbers
 beetles %>% select(c(Month, contains('_'))) #Selects month column, and all other columns which contain _
 beetles %>% select(!Month) #Selects everything, apart from the column month, using ! (meaning apart from)
 beetles %>% select(!c(Month, Site)) #Selects everything apart from the month and site column
+
+beetles %>% 
+  filter(Onthophagus_sideki > 10) #Removes rows that have less than 10 Onthophagus sideki, using the filter function
+beetles %>% 
+  filter(Onthophagus_sideki & Ochicanthon_woroae > 10) #Removes the rows for two of the species rows which are less than 10
+beetles %>%
+  filter(Ochicanthon_woroae > 15, Month == 'July') #Selects only the rows that this species has greater than 15 samples in the month of July
+
+
